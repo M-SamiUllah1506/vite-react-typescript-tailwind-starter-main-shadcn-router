6fbc08eae9b0b0c47d96168cd9c64d5f19904d9d
Muhammad Sami Ullah <sami.suu1506@gmail.com>
2025-08-05 23:18:28 +0500

    chore: mark update as done

--- src/layout/Contactlayout.tsx
+++ src/layout/Contactlayout.tsx
@@ -4,10 +4,10 @@ import { Outlet } from 'react-router-dom';
 const Contactlayout = () => {
   return (
     <div>
-           <Contant/>
-           <Outlet/>
+      <Contant />
+      <Outlet />
     </div>
   )
 }
 
-export default Contactlayout
+export default Contactlayout;
